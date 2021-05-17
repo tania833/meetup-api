5e13be11247ef5c07768e63c88a152db19c588f8
Tetiana Polishchuk <tania.polischuk1998@gmail.com>
2021-05-17 05:46:09 +0000

    Mp 2: Create endpoints for creating, updating and deleting of an event

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/all-events', function(req, res, next) {
-  res.send('Got a GET request');
+var meetingsCollection;
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect(
+    'mongodb+srv://tania833:tania833@cluster0.u3u1g.mongodb.net/Meetup?retryWrites=true&w=majority'
+)
+    .then((client) => {
+        console.log('Connected to Database');
+        const db = client.db('meetup');
+        meetingsCollection = db.collection('meetings');
+    })
+    .catch((error) => console.error(error));
+
+router.get('/all-events', (req, res, next) => {
+    const result = meetingsCollection
+        .find()
+        .toArray()
+        .then(function (result, error) {
+            res.send(result);
+        })
+        .catch((error) => console.error(error));
 });
 
-router.post('/form-input', function (req, res) {
-  res.send('Got a POST request')
+router.post('/event-add', async (req, res) => {
+    try {
+        const result = await meetingsCollection.insertOne(req.body);
+        res.send(result);
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+router.put('/event-update', function (req, res) {
+    res.send('Got a PUT request');
+});
+
+router.delete('/event-delete', function (req, res) {
+    res.send('Got a DELETE reques');
 });
 
 module.exports = router;
